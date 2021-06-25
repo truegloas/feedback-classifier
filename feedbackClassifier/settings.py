bcfab572846a0aef168e31a644429a16ad87321c
truegloas <50672892+truegloas@users.noreply.github.com>
2021-06-25 18:58:57 +0700

    Update settings.py

--- feedbackClassifier/settings.py
+++ feedbackClassifier/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l$u-f9b)8)64t2uvu^r+!qtoi&fnk3u)--9tn)#3c-fsp++wp!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
